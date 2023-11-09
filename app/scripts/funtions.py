7751bd53d76471b2ba9cb31ba9b24e6e42478d96
Taylan Cicek <cicek.onurtaylan@gmail.com>
2023-11-09 16:18:12 -0700

    compression functions added

--- app/scripts/funtions.py
+++ app/scripts/funtions.py
@@ -1,4 +1,4 @@
-from PyPDF2 import PdfWriter
+from PyPDF2 import PdfReader, PdfWriter
 from io import BytesIO
 
 def pdf_merger(pdf_file_list):
@@ -12,3 +12,17 @@ def pdf_merger(pdf_file_list):
     merged_pdf_stream.seek(0)
     
     return merged_pdf_stream
+
+
+def pdf_compress(input_file):
+    reader = PdfReader(input_file)
+    writer = PdfWriter()
+
+    for page in reader.pages:
+        page.compress_content_streams()
+        writer.add_page(page)
+
+    compressed_pdf_stream = BytesIO()
+    writer.write(compressed_pdf_stream)
+    compressed_pdf_stream.seek(0)
+    return compressed_pdf_stream

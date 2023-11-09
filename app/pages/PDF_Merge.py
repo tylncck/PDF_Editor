fd4480439b5237eb3c20f78721ab8663974119e9
Taylan Cicek <cicek.onurtaylan@gmail.com>
2023-11-09 16:17:24 -0700

    Merge page was completed

--- app/pages/PDF_Merge.py
+++ app/pages/PDF_Merge.py
@@ -12,10 +12,7 @@ st.write("Your uploaded files are not saved anywhere, so you can merge your file
 uploaded_files = st.file_uploader('Upload your PDF files (multiple files supported)', type=['pdf'], accept_multiple_files=True)
 
 if uploaded_files:
-    # Create a list to store the UploadedFile objects
     file_data = [{'file': file, 'name': file.name} for file in uploaded_files]
-
-    # Create a list of file names
     file_names = [item['name'] for item in file_data]
 
     st.write('Please sort the files in the order you wish them to be merged.')
@@ -24,11 +21,9 @@ if uploaded_files:
     output_file_name = st.text_input("Enter the merged PDF file name (optional):")
     
     if st.button('Merge PDFs'):
-        # Reorder the file data based on the sorted names
         sorted_file_data = sorted(file_data, key=lambda x: sorted_names.index(x['name']))
         sorted_files = [item['file'] for item in sorted_file_data]
         
-        # Merge the PDFs in the sorted order
         merged_pdf = pdf_merger(sorted_files)
         st.write('Merged the uploaded PDFs successfully. Click the button to download the merged file')
         

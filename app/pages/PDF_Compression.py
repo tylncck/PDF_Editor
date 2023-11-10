78776f8ab20d1aa64fdf3fefa228bafba32d9abf
Taylan Cicek <cicek.onurtaylan@gmail.com>
2023-11-09 18:04:00 -0700

    Compression page finalized

--- app/pages/PDF_Compression.py
+++ app/pages/PDF_Compression.py
@@ -1,15 +1,28 @@
 import streamlit as st
-from scripts.funtions import pdf_compress
+from scripts.funtions import pdf_compress, delete_pdf_files, save_file
 import subprocess
 import io 
 import os
 
 st.title('Compressing PDFs')
 
+st.markdown(
+    """
+    Welcome to the PDF Compression Tool! Choose a compression method and upload your PDF file below. 
+    The tool supports two compression methods: **PyPDF2 Compression** and **Ghostscript Compression**.
+    
+    - **PyPDF2 Compression:** This method uses the PyPDF2 library to compress PDF files.
+    - **Ghostscript Compression:** This method uses Ghostscript to compress PDF files.
+    
+    First upload the PDF file you wish to compress. Then click the **Compress PDF** button to start the compression process. 
+    The compressed file will be available for download once the process is complete.
+    """
+)
+
 # Create a radio button to choose the compression method
 compression_method = st.radio("Select Compression Method", ("PyPDF2 Compression", "Ghostscript Compression"))
 
-uploaded_file = st.file_uploader('Upload your PDF files (multiple files supported)', type=['pdf'], accept_multiple_files=False)
+uploaded_file = st.file_uploader('Upload your PDF file (single file upload supported)', type=['pdf'], accept_multiple_files=False)
 
 if uploaded_file:
     if st.button('Compress PDF'):
@@ -18,14 +31,8 @@ if uploaded_file:
             compressed_pdf = pdf_compress(uploaded_file)
         else:
             target_directory = 'app/files'
-            os.makedirs(target_directory, exist_ok=True)
-
-            target_path = os.path.join(target_directory, uploaded_file.name)
-            with open(target_path, "wb") as file:
-                file.write(uploaded_file.read())
-            
+            target_path = save_file(uploaded_file)
             output_file = target_directory + '/' + output_file_name
-            
             compression_command = f'gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile="{output_file}" "{target_path}"'
             
             subprocess.run(compression_command, shell=True, check=True)
@@ -38,16 +45,7 @@ if uploaded_file:
 
         st.write(f'Compressed the uploaded PDF successfully using the {compression_method}. Initial file size {uploaded_file_size:.2f} MB reduced to {compressed_file_size:.2f} MB. Click the button to download the compressed file.')
 
-        # Remove the saved files
-        files = os.listdir(target_directory)
-
-        # Filter PDF files
-        pdf_files = [file for file in files if file.lower().endswith(".pdf")]
-
-        # Remove PDF files
-        for pdf_file in pdf_files:
-            pdf_path = os.path.join(target_directory, pdf_file)
-            os.remove(pdf_path)
+        delete_pdf_files()
 
         st.download_button('Download Compressed PDF', compressed_pdf, key='download_pdf', file_name=output_file_name)
 
